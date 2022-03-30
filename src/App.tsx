618054dc75cfe4e55a916e9d4614d6ffc4c38d79
marrcelosantana <marcelosantana@alu.ufc.br>
2022-03-29 21:05:26 -0300

    config firebase

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,17 @@
+import { useState } from "react";
+
 function App() {
+  const[counter, setCounter] = useState(0);
+
+  function increment(){
+    setCounter(counter + 1);
+  }
+
   return (
     <div className="App">
-      <h1>Hello World</h1>
+      <button onClick={increment}>{counter}</button>
     </div>
   );
 }
+
 export default App;
