70bb89c8de87437fcb18442e875d191e453c6c3b
marrcelosantana <marcelosantana@alu.ufc.br>
2022-03-30 18:22:02 -0300

    add routes

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,12 @@ import { BrowserRouter, Route } from 'react-router-dom';
 import { Home } from "./pages/Home";
 import { NewRoom } from './pages/NewRoom';
 
-
 function App() {
 
   return (
     <BrowserRouter>
-      <Route path="/" component={Home}></Route>
+      <Route path="/" exact component={Home}/>
+      <Route path="/rooms/new" component={NewRoom}/>
     </BrowserRouter>
   );
 }
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,10 +2,17 @@ import illustrationImg from '../assets/images/illustration.svg';
 import logoImg from '../assets/images/logo.svg';
 import googleIconImg from '../assets/images/google-icon.svg';
 import { Button } from '../components/Button';
+import { useHistory } from 'react-router-dom';
 
 import './../styles/auth.scss';
 
 export function Home () {
+  const history = useHistory();
+
+  function navigateToNewRoom() {
+    history.push('/rooms/new');
+  }
+  
   return (
     <div id="page-auth">
       <aside>
@@ -16,7 +23,7 @@ export function Home () {
       <main>
         <div className="main-content">
           <img src={logoImg} alt="Letmeask"/>
-          <button className="create-room">
+          <button className="create-room" onClick={navigateToNewRoom}>
             <img src={googleIconImg} alt="Logo do Google" />
             Crie sua sala com o google
           </button>
