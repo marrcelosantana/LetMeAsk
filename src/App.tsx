f3672949c3a454d08a92978dd33df0935efdc781
marrcelosantana <marcelosantana@alu.ufc.br>
2022-03-30 18:10:52 -0300

    add react router

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
-// import { Home } from "./pages/Home";
+import { BrowserRouter, Route } from 'react-router-dom';
+import { Home } from "./pages/Home";
 import { NewRoom } from './pages/NewRoom';
-import './styles/global.scss';
+
 
 function App() {
 
   return (
-    <NewRoom/>
+    <BrowserRouter>
+      <Route path="/" component={Home}></Route>
+    </BrowserRouter>
   );
 }
 
