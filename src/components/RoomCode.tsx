97332717b0d014676eec9624a98872ceb435c188
marrcelosantana <marcelosantana@alu.ufc.br>
2022-04-05 00:36:33 -0300

    roomCode and useParams

---
+++ src/components/RoomCode.tsx
@@ -0,0 +1,28 @@
+import copyImg from '../assets/images/copy.svg';
+import toast, { Toaster } from 'react-hot-toast';
+
+import '../styles/roomCode.scss';
+
+type RoomCodeProps = {
+  code: string;
+}
+
+export function RoomCode (props: RoomCodeProps){
+
+  function copyRoomCodeToClipboard() {
+    navigator.clipboard.writeText(props.code);
+    toast.success("Código copiado!");
+  }
+
+  return(
+    <div>
+      <Toaster/>
+      <button className="room-code" onClick={copyRoomCodeToClipboard}>
+        <div>
+          <img src={copyImg} alt="Copy room code" />
+        </div>
+        <span>Sala #{props.code}</span>
+      </button>
+    </div>
+  );
+}
